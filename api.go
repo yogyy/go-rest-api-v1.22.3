65c29ed4665a4b75b4287f0d54d7036a94b71c60
yogyy <yogimr261@gmail.com>
2024-05-20 20:29:22 +0700

    chore: moving to helper add jwt

--- api.go
+++ api.go
@@ -7,26 +7,6 @@ import (
 	"net/http"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	return json.NewEncoder(w).Encode(v)
-}
-
-type apiFunc func(w http.ResponseWriter, r *http.Request) error
-
-type apiError struct {
-	Error string
-}
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			WriteJSON(w, http.StatusBadRequest, apiError{Error: err.Error()})
-		}
-	}
-}
-
 type APIService struct {
 	addr  string
 	store Storage
@@ -39,42 +19,26 @@ func NewAPIServer(addr string, store Storage) *APIService {
 	}
 }
 
-func (s *APIService) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-	req := new(CreateAccountRequest)
-
-	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+func (s *APIService) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	transferReq := new(TransferRequest)
+	if err := json.NewDecoder(r.Body).Decode(transferReq); err != nil {
 		return err
 	}
 
-	account := NewAccount(req.FirstName, req.LastName)
-	if err := s.store.CreateAccount(account); err != nil {
-		return err
-	}
+	defer r.Body.Close()
 
-	return WriteJSON(w, http.StatusOK, account)
-}
-
-func (s *APIService) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	accounts, err := s.store.GetAccounts()
-	if err != nil {
-		return err
-	}
-
-	return WriteJSON(w, http.StatusOK, accounts)
-}
-
-func (s *APIService) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
-	// id := r.PathValue("id")
-	message := map[string]string{"id": r.PathValue("id")}
-	return WriteJSON(w, http.StatusOK, message)
+	return WriteJSON(w, http.StatusOK, transferReq)
 }
 
 func (s *APIService) Run() error {
 	router := http.NewServeMux()
 
 	router.HandleFunc("GET /account", makeHTTPHandleFunc(s.handleGetAccount))
-	router.HandleFunc("GET /account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("GET /account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountByID), s.store))
 	router.HandleFunc("POST /account", makeHTTPHandleFunc(s.handleCreateAccount))
+	router.HandleFunc("DELETE /account/{id}", makeHTTPHandleFunc(s.handleDeleteAccount))
+
+	router.HandleFunc("POST /transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	v1 := http.NewServeMux()
 	v1.Handle("/api/v1/", http.StripPrefix("/api/v1", router))
@@ -87,23 +51,4 @@ func (s *APIService) Run() error {
 	log.Printf("Server has started http://%s", s.addr)
 
 	return server.ListenAndServe()
-}
-
-func ReqLoggerMiddleware(next http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("method %s, path: %s", r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-	}
-}
-
-func RequireAuthMiddleware(next http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if token != "Bearer token" {
-			http.Error(w, "Unauthroized", http.StatusUnauthorized)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	}
 }
---
+++ helper.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type apiError struct {
+	Error string `json:"error"`
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, apiError{Error: err.Error()})
+		}
+	}
+}
+
+func ReqLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("method %s, path: %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
+}
+
+func RequireAuthMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token != "Bearer token" {
+			http.Error(w, "Unauthroized", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func getID(r *http.Request) (int, error) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return id, fmt.Errorf("invalid id given %s", idStr)
+	}
+
+	return id, nil
+}
+
+func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT auth with middleware")
+		header := r.Header.Get("Authorization")
+
+		if len(header) == 0 {
+			permissionDenied(w)
+			return
+		}
+		parts := strings.Split(header, " ")
+
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			permissionDenied(w)
+			return
+		}
+		tokenStr := parts[1]
+
+		// log.Printf("token :%s", tokenStr)
+		token, err := validateJWT(tokenStr)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+		if !token.Valid {
+			permissionDenied(w)
+			return
+		}
+		userId, err := getID(r)
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+		account, err := s.GetAccountByID(userId)
+		if err != nil {
+			// permissionDenied(w)
+			WriteJSON(w, http.StatusForbidden, apiError{Error: "account error"})
+			return
+		}
+		// log.Println(account)
+		claims := token.Claims.(jwt.MapClaims)
+
+		if account.Number != int64(claims["accountNumber"].(float64)) {
+			permissionDenied(w)
+			return
+		}
+
+		if err != nil {
+			permissionDenied(w)
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func createJWT(account *Account) (string, error) {
+	claims := &jwt.MapClaims{
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte("constantine"))
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := "constantine"
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
+}
+
+func permissionDenied(w http.ResponseWriter) {
+	WriteJSON(w, http.StatusForbidden, apiError{Error: "permission denied"})
+}
